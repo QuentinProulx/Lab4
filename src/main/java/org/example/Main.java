bf117a725c62e5fa48ee2d6d5f8a86273eada239
Quentin <quentin.proulx@gmail.com>
2025-04-29 15:33:23 -0400

    Changed values in bubbleSort & added ascending/descending options in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         int[] nums = {3, 1, 5, 2, 1, 4};
-        insertionSort(nums); // {1, 1, 2, 3, 4, 5}
+        bubbleSort(nums); // {1, 1, 2, 3, 4, 5}
 
         System.out.println(Arrays.toString(nums));
     }
@@ -15,16 +15,23 @@ public class Main {
      * Sorts a list of integers in ascending order
      * @param nums the list of integers to be sorted
      */
-    public static void bubbleSort(int[] nums) {
+    public static void bubbleSort(int[] nums, boolean ascending) {
         for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = 0; j < nums.length - i; j++) {
-                if (nums[i] > nums[i + j]) {
-                    int temp = nums[i];
-                    nums[i] = nums[i + j];
-                    nums[i + j] = temp;
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                boolean condition = ascending
+                        ? nums[j] > nums[j + 1]
+                        : nums[j] < nums[j + 1];
+                if (condition) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
                 }
             }
         }
+    }
+
+    public static void bubbleSort(int[] nums) {
+        bubbleSort(nums, true);
     }
 
     /**
